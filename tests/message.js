22e5eb79be98760186fa694e7941a31251165a19
Rob Rodriguez <rob.rodriguez@qwasi.com>
2015-08-04 14:10:50 -0700

    Adding Qwasi App as default

--- tests/message.js
+++ tests/message.js
@@ -1,8 +1,6 @@
 var argv = require('minimist')(process.argv.slice(2));
 
-var qwasi = require('../lib/client')('5575fa17e1910a4f6e3244f3',
-				     'c479a1f1c72b66fa510b7076890f5e01',
-				     { beta: true });
+var qwasi = require('../lib/client')({ beta: true });
 
 qwasi['message.send'](argv.to, argv.alert, argv.message, function(err, response) {
     if (err) {
